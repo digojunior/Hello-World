66c07ec110a7bcd15b0182b8d6947cee35ad4541
Rodrigo Junior <rodrigo_junior@hotmail.com>
2018-06-12 14:37:10 -0300

    atualização

---
+++ Python Programming OOP - Udacity/Projeto filme/calculadora.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class Calculadora:
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def soma(self):
+        return self.num1 + self.num2
+
+    def subtrai(self):
+        return self.num1 - self.num2
+
+    def multiplica(self):
+        return self.num1 * self.num2
+
+    def divide(self):
+        return self.num1 / self.num2
+    
+    
+
